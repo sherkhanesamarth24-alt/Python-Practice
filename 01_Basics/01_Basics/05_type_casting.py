cdc3543ca2305af84d6a98d9f4ce85895ce0d826
Samarth Sherkhane <sherkhanesamarth24@gmail.com>
2026-09-25 16:38:12 +0530

    Update 05_type_casting.py

--- 01_Basics/01_Basics/05_type_casting.py
+++ 01_Basics/01_Basics/05_type_casting.py
@@ -1,1 +1,13 @@
+# Python Type Casting
 
+age = input("Enter your age: ")
+age = int(age)
+
+height = input("Enter your height: ")
+height = float(height)
+
+print("Age:", age)
+print("Age Type:", type(age))
+
+print("Height:", height)
+print("Height Type:", type(height))
